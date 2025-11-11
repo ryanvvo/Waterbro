04901309ee598134d72bfe97c39d65b85db4ea45
Ryan Viet Vo <rvvo@uci.edu>
2025-11-11 11:23:44 -0800

    Tracker deemed obsolete

--- Display.py
+++ Display.py
@@ -5,6 +5,24 @@ class Display:
         self._root = tk.Tk()
         self._root.title("Waterbro")
         self._root.geometry("300x200")
+        self._label = tk.Label(self._root, text="00:00")
+        self._label.pack()
+
+        self._time = 60
+
+    def update(self):
+        self._label.config(text=_timeToStr(self._time))
+
+    def run(self):
+        self.update()
+        self._time -= .1
+        self._root.after(100, self.run)
 
     def mainloop(self):
         self._root.mainloop()
+
+def _timeToStr(time):
+    time = int(time)
+    seconds = time%60
+    minutes = time//60
+    return f"{minutes:02d}:{seconds:02d}"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Display import Display
-from Tracker import Tracker
 
 if __name__ == "__main__":
         display = Display()
-        tracker = Tracker(0)
+        display.run()
+
         display.mainloop()
