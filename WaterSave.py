22c53957ce888c4a399aae287bf066d7115d9ca6
Ryan Vo <ryan.v.vo@gmail.com>
2025-11-16 23:40:22 -0800

    Water save start

---
+++ WaterSave.py
@@ -0,0 +1,20 @@
+import json
+
+DATA = "water_save.json"
+
+class WaterSave:
+    def __init__(self):
+        """
+        Saves the water log and goal to a file to be used to save information between sessions.
+        """
+
+        pass
+
+    def load(self):
+        """ Loads the information from the file. """
+
+    def log_drank(self):
+        """ Logs the water drank to the file. """
+
+    def log_goal(self):
+        """ Logs the water goal to the file. """
