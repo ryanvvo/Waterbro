01d896e02dc3e7e39b48fa4622ecbd7eaf188a06
Ryan Vo <ryan.v.vo@gmail.com>
2026-01-07 23:03:03 -0800

    git ignore

--- Tooltip.py
+++ Tooltip.py
@@ -1,1 +1,7 @@
-# Tooltip.py holds a class that adds a tooltip to a widget.
+# Tooltip.py holds a class that adds a wrapper to a widget that enables tooltips.
+
+import tkinter as tk
+
+class Tooltip:
+    def __init__(self, widget: tk.Widget):
+        pass
